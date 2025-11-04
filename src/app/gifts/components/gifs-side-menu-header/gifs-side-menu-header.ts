34f86be46b62ef4226972f73234e2034cc480749
Jonathan Carpio <jcarpio@swarmingts.com>
2025-11-04 15:26:35 +0100

    Create component to second_task

---
+++ src/app/gifts/components/gifs-side-menu-header/gifs-side-menu-header.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-side-menu-header',
+  imports: [],
+  templateUrl: './gifs-side-menu-header.html',
+})
+export class GifsSideMenuHeader {}
---
+++ src/app/gifts/components/gifs-side-menu-options/gifs-side-menu-options.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-side-menu-options',
+  imports: [],
+  templateUrl: './gifs-side-menu-options.html',
+  styleUrl: './gifs-side-menu-options.css',
+})
+export class GifsSideMenuOptions {
+
+}
