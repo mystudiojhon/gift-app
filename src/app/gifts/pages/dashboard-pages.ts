79f115eca46cffb52d62ac91772e960d63a3f6f6
Jonathan Carpio <jcarpio@swarmingts.com>
2025-11-04 12:54:09 +0100

    first commit to app-gift , complete

---
+++ src/app/gifts/pages/dashboard-pages.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  imports: [],
+  templateUrl: './dashboard-pages.html',
+  styleUrl: './dashboard-pages.css',
+})
+export class DashboardPages {
+
+}
+
+// IGNORE , Para utilizar y crear un componente Angular con angular schematics
+// ng generate component gifts/pages/dashboard-pages --standalone --skip-tests --flat --module=src/app/app.routes.ts
