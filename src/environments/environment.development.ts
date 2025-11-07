639eef9220675a89fb39866c02ef9f7eb71c76c0
Jonathan Carpio <jcarpio@swarmingts.com>
2025-11-07 13:49:22 +0100

    add route enviroment and interfaces

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  companyName: 'STS',
+  companyNameFull: 'Gifs App',
+  companyName2: 'App',
+
+  // Configuración de la API de Giphy
+
+  // API Key pública para desarrollo (se puede usar libremente)
+
+  //URL (v1)
+};
